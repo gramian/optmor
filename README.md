17ddcc1cf861aaad8b36a39a2852c89d7ec1a319
ch <christian.himpe@wwu.de>
2016-02-28 22:50:27 +0100

    release 2.5

--- README.md
+++ README.md
@@ -1,22 +1,125 @@
-optmor
-======
+# optmor
 
-OPTimization-based Model Order Reduction (Version 2.1)
+OPTimization-based Model Order Reduction (Version 2.5) ![DOI badge](https://zenodo.org/badge/doi/10.5281/zenodo.46683.png)
 
 
-License
--------
+## About
 
-All source code is licensed under the open source BSD 2-clause license:
-Copyright (c) 2013-2015, Christian Himpe
+optimization-based model order reduction,
+for the computation of combined state and parameter
+reduced order models of state-space input-output systems
+
+
+## License
+
+All source code is licensed under the open source 
+[BSD 2-clause license](http://opensource.org/licenses/BSD-2-Clause):
+
+#### Copyright (c) 2013-2016, Christian Himpe
 
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
 
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
 
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</p>
+
+
+## Usage
+
+General Usage: `{X,P} = optmor(f,g,s,t,w,pr,nf,ut,us,xs,um,xm);`
+
+Minimal Usage: `{X,P} = optmor(f,g,s,t,w);`
+
+About Info Usage: `v = optmor('version');`
+
+
+## Arguments
+
+`f` - system function handle; signature: xdot = f(x,u,p)
+
+`g` - output function handle; signature: y = g(x,u,p)
+
+`s` - system dimensions [inputs,states,outputs]
+
+`t` - time discretization [step,stop]
+
+`r` - reduced order(>1) or error threshold(<1)
+
+`q` - nominal parameter
+
+
+### Optional Arguments
+
+`nf` - options, 6 components
+
+`ut` - input; default: delta impulse
+
+`x0` - initial state; default: zero
+
+`co` - covariance matrix; default: unit 
+
+`yd` - experimental data; default: empty
+
+
+## Flags
+
+`nf(1)` - Optimization Algorithm: fminunc(0), fminsearch(1), custom(-1)
+
+`nf(2)` - Lasso Regularization Weight: default(0)
+
+`nf(3)` - Tikhonov Regularization Weight: default(0.1)
+
+`nf(4)` - Data-Driven Regularization Weight: default(0)
+
+`nf(5)` - Number of Maximum Optimizer Iterations: default(4)
+
+`nf(6)` - Initial Parameter: last(0), random(1)
+
+
+## References
+
+* C. Himpe, M. Ohlberger; "[Data-driven combined state and parameter reduction for inverse problems](http://dx.doi.org/10.1007/s10444-015-9420-5)"; Advances in Computational Mathematics 41(5): 1343--1364, 2015; [Preprint](), [Source](), [Runmycode]().
+    + One-Sentence Abstract: Extensions for combined state and parameter reduction for inverse problems.
+
+
+## Cite
+
+* Cite as:
+
+    C. Himpe (2016). optmor - Optimization-Based Model Order Reduction (Version 2.5) [Software]. http://gramian.de
+
+* BibTeX:
+
+        @MISC{optmor,
+         author = {C. Himpe},
+         title = {{optmor} - Optimization-Based Model Order Reduction (Version 2.5)},
+         howpublished = {\url{http://gramian.de}},
+         year = {2016}
+        }
+* DOI: [10.5281/zenodo.46683](http://dx.doi.org/10.5281/zenodo.46683) (Version 2.5)
+* Last Change: 2016-02-28
+
+
+## Links
+
+* http://gramian.de emgr - Empirical Gramian Framework
+* http://modelreduction.org Model Order Reduction Wiki
+* http://morepas.org Model Reduction for Parametrized Systems
 
--- optmor.m
+++ optmor.m
@@ -1,71 +1,77 @@
-function XP = optmor(f,g,s,t,r,q,co,nf,ut,x0,yd)
-% optmor (Version 2.1)
-% by Christian Himpe, 2013-2015 ( http://wwwmath.uni-muenster.de/u/himpe )
+function XP = optmor(f,g,s,t,r,q,nf,ut,x0,co,yd)
+% optmor (Version 2.5)
+% by Christian Himpe, 2013-2016 ( wwwmath.uni-muenster.de/u/himpe )
 % released under BSD 2-Clause License ( opensource.org/licenses/BSD-2-Clause )
 %
 % SYNTAX:
-%    W = optmor(f,g,s,t,r,q,[co],[nf],[ut],[x0],[yd]);
+%    W = optmor(f,g,s,t,r,q,[nf],[ut],[x0],[co],[yd]);
 %
 % SUMMARY:
 %    optmor - optimization-based model order reduction,
-%    computation of empirical gramians for model reduction,
-%    system identification and uncertainty quantification.
-%    Enables gramian-based nonlinear model order reduction.
+%    for the computation of combined state and parameter
+%    reduced order models of state-space input-output systems.
 %    Compatible with OCTAVE and MATLAB.
 %
 % ARGUMENTS:
 %   (func handle)  f - system function handle; signature: xdot = f(x,u,p)
 %   (func handle)  g - output function handle; signature:    y = g(x,u,p)
 %        (vector)  s - system dimensions [inputs,states,outputs]
-%        (vector)  t - time discretization [start,step,stop]
-%        (scalar)  r - reduced order or error threshold
-%        (vector)  q - parameter
-%        (matrix,vector,scalar)  [co = 1] - covariance matrix
-%        (vector,scalar) [nf = 0] - options, 10 components:
-%            + Optimization Algorithm: fminunc(0), fminsearch(1), Custom Optimizer(-1)
+%        (vector)  t - time discretization [step,stop]
+%        (scalar)  r - reduced order(>1) or error threshold(<1)
+%        (vector)  q - nominal parameter
+%        (vector,scalar) [nf = 0] - options, 6 components:
+%            + Optimization Algorithm: fminunc(0), fminsearch(1), custom(-1)
 %            + Lasso Regularization Weight: default(0)
-%            + Tikhonov Regularization Weight: default(0)
+%            + Tikhonov Regularization Weight: default(0.1)
 %            + Data-Driven Regularization Weight: default(0)
-%            + Monte-Carlo Basis Enrichment (Base Size)
-%            + Random Seed: none(0)
-%            + State-space trajectory selection: pod-greedy(0), pod(1), pca(2)
-%            + Add x States per Iteration
-%            + Orthogonalization Algorithm: orth(0), qr(1), svd(2)
-%            + Solver: IRK3(0), Custom Solver with handle(-1)
-%        (matrix,vector,scalar,handle)  ut
-%        (vector)  x0 - initial state
-%        (matrix)  yd - experimental data
+%            + Number of Maximum Optimizer Iterations: default(4)
+%            + Initial Parameter: last(0), random(1)
+%        (matrix,vector,scalar,handle)  ut - input; default: delta impulse
+%        (vector,scalar)  x0 - initial state; default: zeros
+%        (matrix,vector,scalar)  [co = 1] - covariance matrix: unit
+%        (matrix)  yd - experimental data: empty
 %
 % RETURNS:
 %              (cell)  XP - {State-,Parameter-} Projection
 %
 % CITATION:
-%    C. Himpe (2015). optmor - Optimization-Based Model Order Reduction (Version 2.1)
-%    [Computer Software]. Available from http://???
+%    C. Himpe (2016). optmor - Optimization-Based Model Order Reduction
+%    (Version 2.5) [Software]. Available from http://gramian.github.io/optmor .
+%    doi: 10.5281/zenodo.46683 .
 %
 % KEYWORDS:
-%    model reduction, combined reduction, greedy
+%    model reduction, combined reduction, greedy sampling
 %*
 
+    % Custom Solver
+    global ODE;
+    if(isa(ODE,'function handle')==0), ODE = @rk2; end;
+
     % Version Info
-    if(nargin==1 && strcmp(f,'version')), XP = 2.1; return; end;
+    if( nargin==1 && strcmp(f,'version') ), XP = 2.5; return; end;
+
+    % Test if Octave
+    if(~exist('OCTAVE_VERSION','builtin')), vec = @(m) m(:); end;
 
     % Default Arguments
-    if(nargin<7  || isempty(co)), co = 1; end; % Assume unit covariance
-    if(nargin<8  || isempty(nf)), nf = 0; end; % Assume default options
-    if(nargin<9  || isempty(ut)), ut = 1; end; % Assume impulse input
-    if(nargin<10 || isempty(x0)), x0 = 0; end; % Assume zero initial state
-    if(nargin<11 || isempty(yd)), yd = 0; end; % Assume no experimental data
+    if( nargin<7  || isempty(nf) ), nf = 0; end; % Assume default options
+    if( nargin<8  || isempty(ut) ), ut = 1.0; end; % Assume impulse input
+    if( nargin<9  || isempty(x0) ), x0 = 0; end; % Assume zero initial state
+    if( nargin<10 || isempty(co) ), co = 1.0; end; % Assume covariance
+    if( nargin<11 || isempty(yd) ), yd = 0; end; % Assume no experimental data
 
     % System Constants
-    J = s(1); % Number of inputs
-    N = s(2); % Number of states
-    O = s(3); % Number of outputs
+    J = s(1);               % Number of inputs
+    N = s(2);               % Number of states
+    O = s(3);               % Number of outputs
+    h = t(1);               % Time step width
+    T = floor(t(2)/h) + 1;  % Number of time steps
+    Q = numel(q);           % Number of parameters
 
-    h = t(2); % Time step width
-    T = round((t(3)-t(1))/h); % Number of time steps
-    r = abs(r); % Ensure positivity
-    Q = numel(q); % Number of parameters
+    % Linear Chirp Input
+    if( isnumeric(ut) && numel(ut)==1 && ut==Inf )
+        ut = @(t) 0.5*cos(pi*(t+10*t.*t))+0.5;
+    end;
 
     % Discretize Procedural Input
     if(isa(ut,'function_handle'))
@@ -76,225 +82,181 @@ function XP = optmor(f,g,s,t,r,q,co,nf,ut,x0,yd)
         end;
     end;
 
-%% LAZY ARGUMENTS
+    % Lazy arguments
+    if(numel(nf)<6),  nf(6) = 0; end;
+    if(numel(ut)==1), ut(1:J,1) = ut./h; end;
+    if(numel(x0)==1), x0(1:N,1) = x0; end;
+    if(numel(co)==1), co(1:Q,1) = co; end;
 
-    if(numel(co)==1), co = co*ones(Q,1); end
-    if(numel(nf)==1), nf = zeros(1,12); end
-    if(numel(ut)==1), ut = [ut*ones(J,1),sparse(J,T-1)]; end
-    if(numel(x0)==1), x0 = x0*ones(N,1); end
+    if(size(ut,2)==1), ut(:,2:T) = 0.0; end;
+    if(size(co,1)==1 || size(co,2)==1), co = spdiags(co,0,Q,Q); end;
 
 %% SETUP
 
-    % SET ABORT CRITERIA
-    if(r >= 1)
+    % Set Abort Critera
+    r = abs(r);
+    if(r >= 1.0)
         n = r;
     else
-        n = N;
-    end
+        n = Q;
+    end;
 
-    % SET DEFAULT TIKHONOV REGULARIZATION
-    if(nf(3)==0),
-        nf(3) = 0.5;
-    end
+    % Set Default Tikhonov Regularization Coefficient
+    if(nf(3)==0 && nf(2)==0)
+        nf(3) = 0.1;
+    end;
 
-    % CHECK FOR DATA
-    if(nf(4) && ( (numel(yd)==1 && yd==0) || size(yd,1)~=O || size(yd,2)~=T) ),
+    % Check for Data if Data-Driven Regularization
+    if(nf(4) && ( (numel(yd)==1 && yd==0) || size(yd,1)~=O || size(yd,2)~=T) )
         error('ERROR! optmor: yd data dimension mismatch!');
     end;
 
-    % SET MINIMUM MONTE-CARLO BASE SIZE
-    if(nf(5)),
-        nf(5) = max(2,round(nf(5)));
-    end
-
-    % SEED RANDOMIZERS
-    if(nf(6)),
-        rand('seed',nf(6));
-        randn('seed',nf(6));
+    % Set Default Maximum Optimizer Iterations
+    if(nf(5)==0)
+        nf(5) = 4;
     end;
 
-    % SET MINIMUM STATE PRINCIPAL COMPONENTS
-    if(nf(8)==0),
-         nf(8) = 1;
-    end
+    % Set up Regularization Operators
+    if(nf(2)~=0 && nf(3)==0 && nf(4)==0)
+        R = @(p,y) nf(2)*norm(p,1);
+    elseif(nf(2)==0 && nf(3)~=0 && nf(4)==0)
+        R = @(p,y) nf(3)*norm(p,2)^2;
+    elseif(nf(2)~=0 && nf(3)~=0 && nf(4)==0)
+        R = @(p,y) nf(2)*norm(p,1) + nf(3)*norm(p,2)^2;
+    elseif(nf(2)==0 && nf(3)==0 && nf(4)~=0)
+        R = @(p,y) nf(4)*t(1)*norm(vec(y-yd),2)^2;
+    elseif(nf(2)~=0 && nf(3)==0 && nf(4)~=0)
+        R = @(p,y) nf(2)*norm(p,1) + nf(4)*t(1)*norm(vec(y-yd),2)^2;
+    elseif(nf(2)==0 && nf(3)~=0 && nf(4)~=0)
+        R = @(p,y) nf(3)*norm(p,2)^2 + nf(4)*t(1)*norm(vec(y-yd),2)^2;
+    elseif(nf(2)~=0 && nf(3)~=0 && nf(4)~=0)
+        R = @(p,y) nf(2)*norm(p,1) + nf(3)*norm(p,2)^2 ...
+                                   + nf(4)*t(1)*norm(vec(y-yd),2)^2;
+    end;
 
-    % Precision Matrix
-    if(size(co,2)==1),
-        W = spdiags(1.0./co,0,Q,Q);
-    else,
-        W = pinv(full(co));
-    end
+%% INIT LOOP
 
-    switch(nf(10)) % ODE Integrator
+    fprintf('optmor progress:\n');
 
-        case 0, % IRK3
-            ode = @irk3;
+    % Set Initial Parameter Projection
+    p = q;
+    P = q./norm(q,2);
 
-        case -1, % Custom
-            global CUSTOM_ODE;
-            ode = CUSTOM_ODE;
-    end
+    % Compute Trajectory for Initial Parameter
+    z = ODE(f,1,t,x0,ut,p);
+    [X,dtemp,vtemp] = svds(z,1);
 
-%% OBJECTIVE FUNCTIONAL SETUP
+    % Set Optimizer Options
+    flags = optimset('Display','off','MaxIter',nf(5));
 
-    b1 = nf(2);
-    b2 = nf(3);
-    bd = nf(4);
+    % Per Interation Timing
+    global TIMINGS;
+    TIMINGS = zeros(Q,1);
 
-    if(b1==0 && b2==0 && bd==0), reg = 0; end; %  NONE
-
-    if(b1~=0 && b2==0 && bd==0), reg = @(p,y) b1*norm1w(p,W); end % L1
-
-    if(b1==0 && b2~=0 && bd==0), reg = @(p,y) b2*norm2w(p,W); end; % L2
-
-    if(b1==0 && b2==0 && bd~=0), reg = @(p,y) bd*norm2t(yd-y,h); end; % DD
-
-    if(b1~=0 && b2~=0 && bd==0), reg = @(p,y) b1*norm1w(p,W) + b2*norm2w(p,W); end; % L1 + L2
-
-    if(b1~=0 && b2==0 && bd~=0), reg = @(p,y) b1*norm1w(p,W) + bd*norm2t(yd-y,h); end % L1 + DD
-
-    if(b1==0 && b2~=0 && bd~=0), reg = @(p,y) b2*norm2w(p,W) + bd*norm2t(yd-y,h); end; % L2 + DD
-
-    if(b1~=0 && b2~=0 && bd~=0), reg = @(p,y) b1*norm1w(p,W) + b2*norm2w(p,W) + bd*norm2t(yd-y,h); end; % L1 + L2 + DD
+    fprintf('|');
 
 %% MAIN LOOP
 
-    % Set Initial Parameter
-    p = q;
-    P = p;
-
-    % Compute Trajectory for Initial Parameter
-    z = ode(f,1,h,T,x0,ut,p);
-    X = prn(z,nf(7),nf(8),1);
-
-    opt = optimset('Display','off');
-
     for I=2:n
 
+        k = tic;
+
+        % Current reduced order model
         fr = @(x,u,p) X'*f(X*x,u,p);
         gr = @(x,u,p) g(X*x,u,p);
         x0r = X'*x0;
-        mmz = @(p,y) reg(p,y) - norm2t(y-ode(f,g,h,T,x0,ut,p),h);
 
-        if(nf(5))
-            mc = orth(rand(Q,nf(5)));
-            p = ones(nf(5),1); % mc\q;
-            J = @(p) mmz(mc*p,ode(fr,gr,h,T,x0r,ut,P*P'*mc*p));
-        else
-            p = q;
-            J = @(p) mmz(p,ode(fr,gr,h,T,x0r,ut,P*P'*p));
-        end
+        % Termination Test
+        if(r < 1.0 && t(1)*norm(vec(ODE(f,g,t,x0,ut,p) ...
+                                   -ODE(fr,gr,t,x0r,ut,P*(P'*p))),2) < r)
+            break;
+        end;
 
-        switch(nf(1)) % Greedy Sampling Algorithm
+        % Set up cost function
+        jf = @(p,y) -t(1)*norm(vec(y-ODE(fr,gr,t,x0r,ut,P*(P'*p))),2)^2 +R(p,y);
+        Jf = @(p) jf(p,ODE(f,g,t,x0,ut,p));       
+
+        % Initial Parameter
+        if(nf(6))
+            p = q + co * randn(Q,1);
+        end;
+
+        % Greedy Sampling Algorithm
+        switch(nf(1))
 
             case 0, % Unconstrained (Quasi-Newton)
-                p = fminunc(J,p,opt);
+                p = fminunc(Jf,p,flags);
 
             case 1, % Derivative-Free (Nelder-Mead)
-                p = fminsearch(J,p,opt);
+                p = fminsearch(Jf,p,flags);
 
             case -1, % Custom Optimizer
-                global CUSTOM_OPT;
-                p = CUSTOM_OPT(J,p);
-        end
+                global FMIN;
+                p = FMIN(Jf,p);
+        end;
 
-        if(nf(5)), p = mc*p; end;
-        P = inc([P,p],nf(9));
+        % Extract and Incorporate New State Base
+        z = ODE(f,1,t,x0,ut,p);
+        z = z - X * (z' * X)';
+        [x,dtemp,vtemp] = svds(z,1);
+        X = gramschmidt(X,x);
 
-        z = ode(f,1,h,T,x0,ut,p);
-        z = z - X*(X'*z);
-        x = prn(z,nf(7),nf(8),1);
-        X = ([X,x]);
+        % Incorporate New Parameter Base
+        P = gramschmidt(P,p);
 
-        if(mod(I,100)==0) fprintf('#'); elseif(mod(I,10)==0), fprintf('+'); else, fprintf('|'); end;
+        TIMINGS(I) = toc(k);
 
-        if(r<1.0)
-            yf = ode(f,g,h,T,x0,ut,q);
-            yr = ode(@(x,u,p) X'*f(X*x,u,p),@(x,u,p) g(X*x,u,p),h,T,X'*x0,ut,P*(P'*q));
-            if(norm2t(yf-yr,h)/norm2t(yf,h) < r), break; end;
-        end
-    end
+        if(mod(I,100)), fprintf('|'); else, fprintf('#'); end;
+    end;
+
+    fprintf('\n');
 
     XP = {X,P};
 end
 
+%% ======== RE-ITERATED GRAM-SCHMIDT ========
+function Q = gramschmidt(Q,v)
 
-%% ======== SQUARED NORMS ========
-function x = norm2t(X,h)
+    m = size(v,2);
 
-    x = h*sum(X(:).*X(:));
-end
+    for I=1:m
+        if(size(Q,2)>=size(Q,1))
+            return;
+        end;
 
-function x = norm1w(X,w)
+        w = v(:,I);
+        b = 0;
 
-    x = sum(abs(w*X));
-end
+        for J=1:10
 
-function x = norm2w(X,w)
+            w = w - Q * (w' * Q)';
+            b = norm(w,2);
+            w = w./b;
+            if(b>0.1)
+                break;
+            end;
+        end;
 
-    x = X'*X; %(X'*w')*(w*X);
-end
-
-function x = norm2(X)
-
-     x = X'*X;
-end
-
-%% ======== STATE-SELECT ========
-function v = prn(u,o,s,w)
-
-    switch(o)
-        case 1, % PCA
-            [v,dummy,dummy2] = svds(bsxfun(@minus,u,mean(u,2),s));
-
-        case 2, % Weighted POD
-            [v,dummy,dummy2] = svds(w*u,s);
-
-        otherwise, % = case 0, % POD
-            [v,dummy,dummy2] = svds(u,s);
-    end;
-end
-
-
-%% ======== INCORPORATE ========
-function v = inc(u,o)
-
-    switch(o)
-        case 1, % QR
-            [v,dummy] = qr(u,0);
-
-        case 2, % SVD
-            [v,dummy,dummy2] = svd(u,'econ');
-
-        otherwise, % ORTH
-            v = orth(u);
+        Q = [Q,w];
     end;
 end
 
 %% ======== DEFAULT ODE INTEGRATOR ========
-function x = irk3(f,g,h,T,z,u,p)
+function x = rk2(f,g,t,z,u,p)
 
     if(isnumeric(g) && g==1), g = @(x,u,p) x; end;
 
-    k1 = h*f(z,u(:,1),p); % 2nd Order Midpoint RK2 for starting value
-    k2 = h*f(z + 0.5*k1,u(:,1),p);
-    z = z + k2;
-    x(:,1) = g(z,u(:,1),p);
+    h = t(1);
+    L = floor(t(2)/h) + 1;
 
-    x(end,T) = 0; % preallocate trajectory
+    x(:,1) = g(z,u(:,end),p);
+    x(end,L) = 0; % preallocate trajectory
 
-    k1 = h*f(z,u(:,2),p); % 2nd start value (improves impulse response)
-    k2 = h*f(z + 0.5*k1,u(:,2),p);
-    z = z + k2;
-    x(:,2) = g(z,u(:,2),p);
-
-    for t=3:T % 3rd Order Improved Runge-Kutta IRK3
-        l1 = h*f(z,u(:,t),p);
-        l2 = h*f(z + 0.5*l1,u(:,t),p);
-        z = z + (2.0/3.0)*l1 + (1.0/3.0)*k1 + (5.0/6.0)*(l2 - k2);
-        x(:,t) = g(z,u(:,t),p);
-        k1 = l1;
-        k2 = l2;
+    for l=2:L % 2nd order Ralston's Runge-Kutta Method
+        k1 = h*f(z,u(:,l-1),p);
+        k2 = h*f(z + 0.666666666666667*k1,u(:,l-1),p);
+        z = z + 0.25*k1 + 0.75*k2;
+        x(:,l) = g(z,u(:,l-1),p);
     end;
 end
 
